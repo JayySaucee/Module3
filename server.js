1d271cdce4a717d45681a455592010072d8cf0f8
Jorge Alejandre <88166019+JayySaucee@users.noreply.github.com>
2022-07-12 14:08:16 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -18,9 +18,40 @@ app.use(express.static('public'));
 
 // Add your code here
 
+function findStockBySymbol(company) {
+     return Object.keys(stocks).includes(company);
+}
 
+function findStockByPrice(price) {
+    const userChoice = undefined;
+    if(price === high)
+    {
+    for(let i = 0; i < stocks.length; i++) {
+            if(stocks.price[i] > stocks.price[i+1]) {
+            userChoice = stocks[i];
+            }
+        }
+    }
 
+    else {
+        for(let j = 0; j < stocks.length; j++) {
+            if(stocks.price[i] < stocks.price[i+1]) {
+                userChoice = stocks[i];
+            }
+        }
+    }
+    return userChoice
+}
 
+app.post("/", (req, res) => {
+    const stockChoice = findStockBySymbol(req.body.companyName);
+    res.send(`You placed an order to buy ` + req.body.query + ` stocks of ${stockChoice.company}. The price of one stock is ${stockChoice.price}. The total for this order is `);
+});
+
+app.post("/", (req, res) => {
+    const priceChoice = findStockByPrice(req.body.price);
+    res.send(`${priceChoice}`);
+ })
 
 // Note: Don't add or change anything below this line.
 app.listen(PORT, () => {
